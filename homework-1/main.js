ca229dc9e3172a55cdb937548e7c6c2829b5a6cb
AnyaMar <markevych.anya@gmail.com>
2022-02-17 12:20:03 +0200

    all bugs fixed

--- homework-1/main.js
+++ homework-1/main.js
@@ -1,6 +1,6 @@
-let milkPrice = 15.678;
-let teaPrice = 123.965;
-let yogurtPrice = 90.2345;
+const milkPrice = 15.678;
+const teaPrice = 123.965;
+const yogurtPrice = 90.2345;
 
 const MAX_PRICE = Math.max(milkPrice, teaPrice, yogurtPrice);
 console.log(MAX_PRICE);
@@ -11,17 +11,13 @@ console.log(MIN_PRICE);
 let sum = milkPrice + teaPrice + yogurtPrice;
 console.log(sum);
 
-const NEW_MILK_PRICE = Math.floor(milkPrice);
-const NEW_TEA_PRICE  = Math.floor(teaPrice);
-const NEW_YOGURT_PRICE = Math.floor(yogurtPrice);
-
-const NEW_SUM = NEW_MILK_PRICE + NEW_TEA_PRICE + NEW_YOGURT_PRICE;
+const NEW_SUM = Math.floor(milkPrice) + Math.floor(teaPrice) + Math.floor(yogurtPrice);
 console.log(NEW_SUM);
 
 const SUM_ROUNDED_TO_HUNDREDS = Math.round(sum/100) * 100;
 console.log(SUM_ROUNDED_TO_HUNDREDS); 
 
-const EVEN = (NEW_SUM % 2 === 0);
+const EVEN = NEW_SUM % 2 === 0;
 console.log(`${EVEN} парне число`);
 
 const REST = 500 - sum;
@@ -31,16 +27,15 @@ const averageNumber = (sum / 3).toFixed(2);
 console.log(averageNumber);
 
 
-let discount = Math.round(Math.random() * (90 - 10) + 10);
+let discount = Math.round(Math.random() * 80 + 10);
 
 
-const sumWithDiscount = sum -(sum/100 * discount);
+const sumWithDiscount = sum -(sum / 100 * discount);
 console.log(`${sumWithDiscount.toFixed(2)} сума до сплати з випадковою знижкою`);
 
 
-let netProfit;
 const cost = Math.floor(sumWithDiscount) / 2;
-netProfit = cost - Math.round(Math.floor(sumWithDiscount)/100 * discount);
+let netProfit = cost - Math.round(Math.floor(sumWithDiscount)/100 * discount);
 console.log(`${netProfit} це чистий прибуток`);
 
 

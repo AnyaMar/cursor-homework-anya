ef6801fa7118776d8724855b35504cb5d4353886
AnyaMar <markevych.anya@gmail.com>
2022-07-18 17:09:20 +0300

    done homework

---
+++ homework-13/main.js
@@ -0,0 +1,34 @@
+function * createIdGenerator() {
+  for(let i = 1; i < Infinity; i++) {
+    yield i;
+  }
+}
+
+const idGenerator = createIdGenerator();
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+
+function* newFontGenerator(size) {
+let word = yield size;
+while(1) {
+  switch(word) {
+    case 'up': 
+    word = yield size += 2;
+    break;
+    case 'down':
+    word = yield size -= 2;
+    break;
+    default:
+    word = yield size;
+  }
+}
+}
+
+const fontGenerator = newFontGenerator(14);
+console.log(fontGenerator.next('up'));
+console.log(fontGenerator.next('down'));
+console.log(fontGenerator.next('up'));
+console.log(fontGenerator.next('up'));
